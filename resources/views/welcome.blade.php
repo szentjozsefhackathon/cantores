4141df21a626eaf6aa22c9d9f48bcb808642a4be
Bertalan Fodor <fodber@organum.hu>
2026-02-09 18:14:59 +0000

    Add liturgical info component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,6 +52,9 @@
         </header>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+                <div class="w-full">
+                    <livewire:liturgical-info />
+                </div>
             </main>
         </div>
 
