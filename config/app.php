01155ac032b233cdbc6f75f0eb9981f37b17a231
Bertalan Fodor <fodber@organum.hu>
2026-02-11 16:01:24 +0000

    Some localization

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
     'locale' => env('APP_LOCALE', 'hu'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'hu'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
