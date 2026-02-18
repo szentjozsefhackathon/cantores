ca2cab01e72e7aa7bdd987a78ccd054632181ab8
Bertalan Fodor <fodber@organum.hu>
2026-02-18 12:10:39 +0000

    Prepare prod release step 1

--- config/admin.php
+++ config/admin.php
@@ -3,4 +3,6 @@
 return [
     // The admin user's email address (set via .env)
     'email' => env('ADMIN_EMAIL'),
+    // The admin user's password (set via .env - not set for production)
+    'password' => env('ADMIN_PASSWORD'),
 ];
