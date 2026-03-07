6d381c686d802a3746e0b3ec772379b7d8f379d0
Bertalan Fodor <fodber@organum.hu>
2026-03-07 08:50:14 +0000

    refactor: update docker-compose and vite configuration to use environment variables for ports and improve healthcheck command

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,31 @@ import {
 } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        cors: true,
-        hmr: {
-            host: 'localhost'
-        },
-        host: '0.0.0.0',
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const port = parseInt(env.VITE_EXTERNAL_PORT) || 5173;
 
-        watch: {
-            ignored: ['**/storage/framework/views/**']
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+        server: {
+            cors: true,
+            hmr: {
+                host: 'localhost'
+            },
+            host: '0.0.0.0',
+            port: port,
+
+            watch: {
+                ignored: ['**/storage/framework/views/**', '**/vendor/**']
+            },
         },
-    },
+    };
 });
