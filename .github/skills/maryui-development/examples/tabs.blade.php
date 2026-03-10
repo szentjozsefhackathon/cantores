9dea0a330464bb6aee97309931548f0092d606d1
Bertalan Fodor <fodber@organum.hu>
2026-03-10 07:21:57 +0000

    track skills

---
+++ .github/skills/maryui-development/examples/tabs.blade.php
@@ -0,0 +1,13 @@
+<x-mary-tabs wire:model="selectedTab">
+    <x-mary-tab name="users-tab" label="Users" icon="o-users">
+        <div>Users</div>
+    </x-mary-tab>
+    <x-mary-tab name="tricks-tab" label="Tricks" icon="o-sparkles">
+        <div>Tricks</div>
+    </x-mary-tab>
+    <x-mary-tab name="musics-tab" label="Musics" icon="o-musical-note">
+        <div>Musics</div>
+    </x-mary-tab>
+</x-mary-tabs>
+
+<x-button label="Change to Musics" @click="$wire.selectedTab = 'musics-tab'" />
