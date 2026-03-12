02b63156ad475035bd8a664c3543625da3987811
Bertalan Fodor <fodber@organum.hu>
2026-03-12 14:47:58 +0000

    feat: update font stylesheet to include additional font weights for improved typography

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,7 +10,7 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <!--link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /-->
 
-    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=inter:400,400i,500,500i,600,600i,700&display=swap" rel="stylesheet" />
 
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
