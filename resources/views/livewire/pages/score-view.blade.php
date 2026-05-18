f9a07799c0daffa6c352fe565a161817322e3c9a
Bertalan Fodor <fodber@organum.hu>
2026-05-18 21:53:05 +0000

    feat: add Aretino settings and preview functionality

--- resources/views/livewire/pages/score-view.blade.php
+++ resources/views/livewire/pages/score-view.blade.php
@@ -270,6 +270,70 @@
                     </div>
                 </div>
 
+                {{-- Aretino Settings Toolbar --}}
+                <div x-show="$wire.format === 'aretino'" x-cloak class="flex flex-wrap items-center gap-x-3 gap-y-2 rounded-lg border border-zinc-200 bg-zinc-50 px-3 py-2 dark:border-zinc-700 dark:bg-zinc-800/50">
+                    <div class="flex items-center gap-1">
+                        <flux:tooltip :content="__('Zoom (%)')">
+                            <flux:icon name="zoom-in" variant="micro" class="shrink-0 text-zinc-500 dark:text-zinc-400" />
+                        </flux:tooltip>
+                        <flux:input size="sm" type="number" x-model="aretinoZoom" min="50" max="300" step="5" class="w-16" />
+                    </div>
+
+                    <div class="flex items-center gap-1">
+                        <flux:tooltip :content="__('Staff size (%)')">
+                            <flux:icon name="list-chevrons-up-down" variant="micro" class="shrink-0 text-zinc-500 dark:text-zinc-400" />
+                        </flux:tooltip>
+                        <flux:input size="sm" type="number" x-model="aretinoStaffSize" min="30" max="300" step="5" class="w-16" />
+                    </div>
+
+                    <div class="flex items-center gap-1">
+                        <flux:tooltip :content="__('Lyric size (pt)')">
+                            <flux:icon name="a-large-small" variant="micro" class="shrink-0 text-zinc-500 dark:text-zinc-400" />
+                        </flux:tooltip>
+                        <flux:input size="sm" type="number" x-model="aretinoLyricSize" min="8" max="60" step="1" class="w-16" />
+                    </div>
+
+                    <div class="flex items-center gap-1">
+                        <flux:tooltip :content="__('Font')">
+                            <flux:icon name="type-outline" variant="micro" class="shrink-0 text-zinc-500 dark:text-zinc-400" />
+                        </flux:tooltip>
+                        <flux:select size="sm" x-model="aretinoLyricFont" class="w-32 text-xs">
+                            <flux:select.option value="'Palatino Linotype', 'Book Antiqua', Palatino, serif">Palatino</flux:select.option>
+                            <flux:select.option value="'Barlow Condensed', sans-serif">Barlow Condensed</flux:select.option>
+                            <flux:select.option value="Garamond, 'EB Garamond', serif">Garamond</flux:select.option>
+                            <flux:select.option value="'Times New Roman', Times, serif">Times New Roman</flux:select.option>
+                            <flux:select.option value="'Franklin Gothic Book', 'Franklin Gothic Medium', 'ITC Franklin Gothic', Arial, sans-serif">Franklin Gothic</flux:select.option>
+                        </flux:select>
+                    </div>
+
+                    <div class="flex items-center gap-1">
+                        <flux:tooltip :content="__('Page ratio')">
+                            <flux:icon name="proportions" variant="micro" class="shrink-0 text-zinc-500 dark:text-zinc-400" />
+                        </flux:tooltip>
+                        <flux:select size="sm" x-model="aretinoPageRatio" class="w-20 text-xs">
+                            <flux:select.option value="auto">{{ __('Auto') }}</flux:select.option>
+                            <flux:select.option value="16/9">16:9</flux:select.option>
+                            <flux:select.option value="4/3">4:3</flux:select.option>
+                            <flux:select.option value="1/1">1:1</flux:select.option>
+                        </flux:select>
+                    </div>
+                </div>
+
+                {{-- Aretino Preview --}}
+                <div x-show="$wire.format === 'aretino'" x-cloak class="mt-4">
+                    <div x-ref="aretinoPreview" class="min-h-16 space-y-4"></div>
+
+                    <div class="mt-2 flex flex-wrap items-center justify-end gap-2" x-show="hasPages">
+                        <span x-show="copyFeedback" x-text="copyFeedback" x-transition class="text-sm text-zinc-600 dark:text-zinc-300"></span>
+                        <flux:button icon="clipboard" variant="ghost" x-on:click="copyImage()">
+                            {{ __('Copy as Image') }}
+                        </flux:button>
+                        <flux:button icon="arrow-down-tray" variant="ghost" x-on:click="exportPng()">
+                            {{ __('Export PNG') }}
+                        </flux:button>
+                    </div>
+                </div>
+
                 {{-- ChordPro Preview --}}
                 <div x-show="$wire.format === 'chordpro'" x-cloak class="mt-4">
                     <div x-ref="chordproPreview" class="min-h-16 space-y-4"></div>
